52bf5548bfa75969cdf16522fcca2da910950dc9
Kamil Zurek <theoriginalpole@gmail.com>
2021-11-18 04:14:11 +0000

    Final final

--- pages/works/discord-bot.js
+++ pages/works/discord-bot.js
@@ -1,14 +1,12 @@
 import {
   Container,
   Badge,
-  Link,
   List,
   ListItem,
   UnorderedList,
   Text
 } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Title, WorkImage, Meta } from '../../components/work'
+import { Title, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
