ff6b3461655232718032ba96fc05e4dd2ca7df92
Kamil Zurek <kamil.zurek@siemens.com>
2021-11-19 08:52:27 +0000

    Custom Gallery Component

---
+++ components/gallery.js
@@ -0,0 +1,53 @@
+import { Image, SimpleGrid } from '@chakra-ui/react'
+import { useCallback, useState } from 'react'
+import Carousel, { Modal, ModalGateway } from 'react-images'
+
+const Gallery = props => {
+  const [currentImage, setCurrentImage] = useState(0)
+  const [viewerIsOpen, setViewerIsOpen] = useState(false)
+
+  const { images } = props
+
+  const openLightbox = useCallback(event => {
+    if (event.target.id) {
+      console.log(event.target.id)
+      setCurrentImage(event.target.id)
+      setViewerIsOpen(true)
+    }
+  }, [])
+
+  const closeLightbox = () => {
+    setCurrentImage(0)
+    setViewerIsOpen(false)
+  }
+
+  return (
+    <>
+      <ModalGateway>
+        {viewerIsOpen && (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={images.map(x => ({
+                source: x.src,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        )}
+      </ModalGateway>
+      <SimpleGrid
+        pt="20px"
+        columns={[2, null, 3]}
+        spacing="10px"
+        onClick={openLightbox}
+      >
+        {images.map((val, index) => (
+          <Image key={index} alt={val.title} id={index} src={val.src} />
+        ))}
+      </SimpleGrid>
+    </>
+  )
+}
+
+export default Gallery
--- pages/works/slime-time.js
+++ pages/works/slime-time.js
@@ -5,16 +5,13 @@ import {
   ListItem,
   UnorderedList,
   Text,
-  Image,
-  VStack,
-  SimpleGrid
+  VStack
 } from '@chakra-ui/react'
 import { Title, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 import ReactPlayer from 'react-player'
-import { useCallback, useState } from 'react'
-import Carousel, { Modal, ModalGateway } from 'react-images'
+import Gallery from '../../components/gallery'
 
 const images = [
   {
@@ -56,37 +53,8 @@ const images = [
 ]
 
 const Work = () => {
-  const [currentImage, setCurrentImage] = useState(0)
-  const [viewerIsOpen, setViewerIsOpen] = useState(false)
-
-  const openLightbox = useCallback(event => {
-    if (event.target.id) {
-      console.log(event.target.id)
-      setCurrentImage(event.target.id)
-      setViewerIsOpen(true)
-    }
-  }, [])
-
-  const closeLightbox = () => {
-    setCurrentImage(0)
-    setViewerIsOpen(false)
-  }
-
   return (
     <Layout title="Slime Time">
-      <ModalGateway>
-        {viewerIsOpen && (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              currentIndex={currentImage}
-              views={images.map(x => ({
-                source: x.src,
-                caption: x.title
-              }))}
-            />
-          </Modal>
-        )}
-      </ModalGateway>
       <Container>
         <Title>
           Slime Time <Badge>2019</Badge>
@@ -116,16 +84,7 @@ const Work = () => {
           controls={true}
           style={{ marginTop: '20px' }}
         />
-        <SimpleGrid
-          pt="20px"
-          columns={[2, null, 3]}
-          spacing="10px"
-          onClick={openLightbox}
-        >
-          {images.map((val, index) => (
-            <Image key={index} alt={val.title} id={index} src={val.src} />
-          ))}
-        </SimpleGrid>
+        <Gallery images={images} />
       </VStack>
       <List ml={4} my={4}>
         <ListItem>
